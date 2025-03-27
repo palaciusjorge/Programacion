f10fc610ff1c17df0fe353e629afdd3497b0958a
palaciusjorge <palaciusjorge@gmail.com>
2025-03-27 18:26:56 +0100

    Segundo ejercicio sesion6

--- src/Sesion6/MainSesion6.java
+++ src/Sesion6/MainSesion6.java
@@ -15,6 +15,16 @@ public class MainSesion6 {
             System.out.println("Introduzca otro numero: ");
             numero = scn1.nextInt();
         }
+        /*EJERCICIO2
+        Escribe un programa que muestre los números del 1 al 10, excepto los múltiplos de 3. Usa un bucle for con continue.
+         */
+        for(int i=1;i<=10;i++){
+            if(i%3 == 0){
+                continue;
+            }
+            System.out.println(i);
+        }
+
 
 
 
