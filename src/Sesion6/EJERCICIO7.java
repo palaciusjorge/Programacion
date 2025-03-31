b64efb5ec20e0cb739c3835e3363271e1fb45c73
palaciusjorge <palaciusjorge@gmail.com>
2025-03-31 19:46:33 +0200

    Primeros 6 ejercicios sesion 6 separados

---
+++ src/Sesion6/EJERCICIO7.java
@@ -0,0 +1,24 @@
+package Sesion6;
+
+import java.util.Scanner;
+
+public class EJERCICIO7 {
+    public static void main(String[] args) {
+        /*EJERCICIO 7 : Haz un programa que intente dividir dos números.
+        Usa try-catch-finally para mostrar siempre un mensaje final que diga "Operación terminada".
+         */
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduzca el dividendo :");
+        int dividendo = scanner.nextInt();
+        System.out.println("Introduzca el divisor :");
+        int divisor = scanner.nextInt();
+        try{
+            int resultado = dividendo/divisor;
+            System.out.println("El resultado de la division es : " + resultado);
+        } catch(ArithmeticException ex){
+            System.err.println("Error: No esta permitido dividir por 0");
+        } finally{
+            System.out.println("Operación terminada.");
+        }
+    }
+}
